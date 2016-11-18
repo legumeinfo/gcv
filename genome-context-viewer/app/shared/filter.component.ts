65d5e3bac0b4475fc37e4d0c4fa42c02c364bf11
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 16:32:54 -0700

    Added filter component

---
+++ genome-context-viewer/app/shared/filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'filter',
+  templateUrl: 'filter.component.html',
+  styles: [ '' ]
+})
+
+export class FilterComponent { }
