ddb53b09cb8a1c4c5919c96c6560e1a19b7ee687
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-17 19:18:40 -0700

    Added left slider component

---
+++ genome-context-viewer/app/shared/left-slider.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'left-slider',
+  templateUrl: 'left-slider.component.html',
+  styleUrls: [ 'left-slider.component.css' ]
+})
+
+export class LeftSliderComponent implements OnInit {
+  ngOnInit(): void {
+    // get filter data
+  }
+}
