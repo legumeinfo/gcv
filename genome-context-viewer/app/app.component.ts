e97b02406f84ba07fe33773e3c50a88bef7807ef
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-17 18:46:49 -0700

    Removed unnecessary tags and styling from app component

--- genome-context-viewer/app/app.component.ts
+++ genome-context-viewer/app/app.component.ts
@@ -3,17 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'gcv',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  template: `<router-outlet></router-outlet>`,
   styleUrls: [ 'app.component.css' ]
 })
 
-export class AppComponent {
-  title = 'Tour of Heroes';
-}
+export class AppComponent { }
