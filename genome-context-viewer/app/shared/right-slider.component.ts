c23a8c58a187688f875324cc03c6f243d94b1755
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 11:10:51 -0700

    Added right slider component

---
+++ genome-context-viewer/app/shared/right-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'right-slider',
+  templateUrl: 'right-slider.component.html',
+  styleUrls: [ 'right-slider.component.css' ]
+})
+
+export class RightSliderComponent { }
