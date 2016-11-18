876b280f5b04a3d25dd1072480c1c55aff10639f
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 13:22:48 -0700

    Added header component

---
+++ genome-context-viewer/app/shared/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'header',
+  templateUrl: 'header.component.html',
+  styleUrls: [ 'header.component.css' ]
+})
+
+export class HeaderComponent { }
