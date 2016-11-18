abfdbb8fc0e3b35ddff26fff057cc63f5bff26b6
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 11:20:48 -0700

    Added local-global-plots component to search component

---
+++ genome-context-viewer/app/components/search/local-global-plots.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'local-global-plots',
+  templateUrl: 'local-global-plots.component.html',
+  styles: [ '' ]
+})
+
+export class LocalGlobalPlotsComponent implements OnInit {
+  ngOnInit(): void {
+    // get local plot data?
+  }
+}
