5c179104930e46b853501046e804e9df5acf6e90
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 16:35:46 -0700

    Added footer component to basic and search components

--- genome-context-viewer/app/shared/footer.component.ts
+++ genome-context-viewer/app/shared/footer.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'filter',
-  template: '',
-  styles: [ '' ]
+  selector: 'footer',
+  templateUrl: 'footer.component.html',
+  styleUrls: [ 'footer.component.css' ]
 })
 
-export class FilterComponent implements OnInit {
-  ngOnInit(): void {
-    // get filter data
-  }
-}
+export class FooterComponent { }
