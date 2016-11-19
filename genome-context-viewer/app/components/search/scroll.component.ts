34e439ab43df5f89e586e052488ef99f1ef7fd8a
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 17:27:21 -0700

    Added scroll component

---
+++ genome-context-viewer/app/components/search/scroll.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'scroll',
+  templateUrl: 'scroll.component.html',
+  styles: [ '' ]
+})
+
+export class ScrollComponent { }
