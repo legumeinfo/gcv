9fe559e8345a97afcaae8837876fb07d7a90f0b8
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 17:48:09 -0700

    Added help component

---
+++ genome-context-viewer/app/shared/help.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'help',
+  template: '<button type="button" class="btn btn-default navbar-btn">Help</button>',
+  styles: [ '' ]
+})
+
+export class HelpComponent { }
