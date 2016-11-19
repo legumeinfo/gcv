479f1770af03b98b37cf293b1440332dfe44b959
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-18 17:43:01 -0700

    Added parameters-toggle component

---
+++ genome-context-viewer/app/shared/parameters-toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'parameters-toggle',
+  template: '<button type="button" class="btn btn-default navbar-btn">Parameters</button>',
+  styles: [ '' ]
+})
+
+export class ParametersToggleComponent { }
