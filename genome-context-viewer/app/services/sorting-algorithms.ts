820ede25a71931fa7df87d9406ce3cf3e1c4bd3d
Alan Cleary <alan.cleary@msu.montana.edu>
2016-11-30 14:54:49 -0700

    Added sorting algorithms structure

---
+++ genome-context-viewer/app/services/sorting-algorithms.ts
@@ -0,0 +1,14 @@
+import { Algorithm } from './algorithm';
+
+export const SORTING_ALGORITHMS: Algorithm[] = [
+  {
+    id: 'chromosome',
+    name: 'Chromosome name',
+    algorithm: function () { }
+  },
+  {
+    id: 'distance',
+    name: 'Edit distance',
+    algorithm: function () { }
+  }
+]
