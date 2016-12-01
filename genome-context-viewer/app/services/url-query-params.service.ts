ba5884aa82f77aa5455a035ff0c4ab6330da2ac8
Alan Cleary <alan.cleary@msu.montana.edu>
2016-12-01 15:31:54 -0700

    Wired url-query-params service to store

--- genome-context-viewer/app/services/url-query-params.service.ts
+++ genome-context-viewer/app/services/url-query-params.service.ts
@@ -1,7 +1,9 @@
 // Angular
-import { Injectable }    from '@angular/core';
-import { Store }         from '@ngrx/store';
-import { Observable }    from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { Injectable }     from '@angular/core';
+import { Observable }     from 'rxjs/Observable';
+import { Router }         from '@angular/router';
+import { Store }          from '@ngrx/store';
 import 'rxjs/add/operator/map';
 
 // App store
@@ -12,18 +14,28 @@ import { UrlQueryParams } from '../models/url-query-params.model';
 export class UrlQueryParamsService {
   params: Observable<UrlQueryParams>;
 
-  constructor(private store: Store<AppStore>) {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private store: Store<AppStore>) {
     this.init();
   }
 
   init(): void {
-    // Bind an observable of our params to "UrlQueryParamsService"
     this.params = this.store.select('url-query-params');
+    this.route.queryParams.subscribe(this.updateStore);
+  }
 
+  private updateUrl(params: any): void {
+    this.router.navigate([], {queryParams: Object.assign(this.params, params)});
+  }
+
+  private updateStore(params: any): void {
+    this.store.dispatch({type: 'ADD_QUERY_PARAMS', payload: params});
   }
 
   // params = QueryParams || AlignmentParams || {order: OrderAlgorithm.id}
   updateParams(params: any): void {
-    this.store.dispatch({type: 'ADD_QUERY_PARAMS', payload: params});
+    this.updateUrl(params);
+    this.updateStore(params);
 	}
 }
