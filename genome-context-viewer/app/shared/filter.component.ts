d167756bd7102623620b49fcffb1f22232e4eda7
Alan Cleary <alan.cleary@msu.montana.edu>
2016-12-05 15:46:17 -0700

    Moved filter template to component template string

--- genome-context-viewer/app/shared/filter.component.ts
+++ genome-context-viewer/app/shared/filter.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'filter',
-  templateUrl: 'filter.component.html',
+  template: `
+    <form class="navbar-form navbar-left">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="e.g. name1|name4|name2">
+      </div>
+      <button type="submit" class="btn btn-default">Filter</button>
+    </form>
+  `,
   styles: [ '' ]
 })
 
