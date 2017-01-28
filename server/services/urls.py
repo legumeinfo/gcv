37898788a61fe9025d9103eed7dbd1e91d17613a
Alan Cleary <alan.cleary@cs.montana.edu>
2017-01-27 19:01:41 -0600

    Fixed typo in basic micro-tracks urls

--- server/services/urls.py
+++ server/services/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('services.views',
     # basic micro-synteny tracks
-    url(r'^micro-synteny-basic/$', 'micro_synteny_basic_tracks'),
-    url(r'^basic_tracks_tree_agnostic/$', 'micro_synteny_basic_tracks'),
+    url(r'^micro-synteny-basic/$', 'micro_synteny_basic'),
+    url(r'^basic_tracks_tree_agnostic/$', 'micro_synteny_basic'),
     # gene to query
     url(r'^micro-synteny-gene-to-query/$', 'micro_synteny_gene_to_query'),
     url(r'^gene_to_query/$', 'micro_synteny_gene_to_query'),
