2f6b110c11a4b33f048bf930b18023320d47d5c1
Alan Cleary <alan.cleary@msu.montana.edu>
2017-02-13 13:06:43 -0700

    Added error logging to server; errors are now saved to errors.log in server root directory

--- server/server/settings.py
+++ server/server/settings.py
@@ -156,3 +156,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# enable error logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'errors.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
