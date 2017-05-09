3ad75cd6c5368e7f988fa28dfac6e0ec8b94104e
Alan Cleary <alan.cleary@msu.montana.edu>
2017-05-09 12:03:09 -0600

    Changed macro-synteny default coloring scheme to alternate between black and black stripes

--- client/assets/js/gcv/synteny.js
+++ client/assets/js/gcv/synteny.js
@@ -258,8 +258,17 @@ GCV.Synteny = class {
   _drawTrack(i) {
     var obj = this,
         //c = this._COLORS[i % this._COLORS.length],
-        c = i % 2 ? '#C0C0C0' : '#000000',
         t = this.data.tracks[i];
+		this.viewer.append('defs')
+		  .append('pattern')
+		    .attr('id', 'diagonalHatch')
+		    .attr('patternUnits', 'userSpaceOnUse')
+		    .attr('width', 4)
+		    .attr('height', 4)
+		  .append('path')
+		    .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
+		    .attr('stroke', '#000000')
+		    .attr('stroke-width', 1);
     // create the track's rows of blocks
     this._blocksToRows(t.blocks);
   	// create the track
@@ -318,7 +327,7 @@ GCV.Synteny = class {
     // draw the blocks
   	var polygons = blocks.append('polygon')
       .attr('class', 'block')
-  	  .style('fill', c)
+  	  .style('fill',  i % 2 ? '#000000' : 'url(#diagonalHatch)')
       .attr('points', function (b) {
         var yTop = ((obj._BLOCK_HEIGHT + obj._PAD) * b.y) + obj._PAD,
             yBottom = yTop + obj._BLOCK_HEIGHT,
