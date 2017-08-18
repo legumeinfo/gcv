0cba99070efc1e864df837aeb173819387e9118f
Alan Cleary <alan.cleary@msu.montana.edu>
2017-08-18 15:13:42 -0600

    Implemented Viterbi algorithm (not tested)

--- client/assets/js/gcv/graph.js
+++ client/assets/js/gcv/graph.js
@@ -548,6 +548,9 @@ Graph.MSAHMM.MatchState = class extends Graph.MSAHMM.State {
       this.emissionProbabilities[o] = p;
     });
   }
+  emit(o) {
+    return this.emissionProbabilities[o];
+  }
   //addPath(pId, o) {
   //  super.addPath(pId);
   //  this.counts[o]       += 1;
@@ -561,15 +564,96 @@ Graph.MSAHMM.MatchState = class extends Graph.MSAHMM.State {
   //}
 }
 
-Graph.viterbi = function(hmm, seq) {
-  var probabilities = {},
-      pointers      = {};
+Graph.MSAHMM.viterbi = function(hmm, seq) {
+  var probs = {},
+      ptrs  = {};
   for (var id in hmm.nodes) {
     if (hmm.nodes.hasOwnProperty(id)) {
-      probabilities[id] = {};
-      pointers[id]      = {};
+      probs[id] = {};
+      ptrs[id]  = {};
     }
   }
+  // a generic probability forward propagate function
+  var propagate = function(from, to, i) {
+    var current   = probs[to][i] || -Infinity,
+        candidate = probs[from][i - !to.startsWith("d")] +  // arithmetic HACK!
+                    Math.log(hmm.getEdge(from, to));
+    if (to.startsWith("m")) {
+      candidate += hmm.getNode(to).attr.emit(seq[i]);
+    }
+    // TODO: use preferred transitions to resolve ties
+    if (candidate > current || (candidate == current && from.startsWith("m"))) {
+      probs[to][i] = candidate;
+      ptrs[to][i]  = from;
+    }
+  }
+  // recursively identifies the sequence's most probable path through the HMM
+  var traceback = function(id, i) {
+    if (id == "s") {
+      return [id];
+    }
+    var ptr = ptrs[id][i],
+        path = traceback(ptrs, i - !ptr.startsWith("d"));  // arithmetic HACK!
+    path.push(id);
+    return path;
+  }
+  // seed start state
+  var s = "s";
+  probs[s][-1] = 0;  // = log(1)
+  // propagate pre-sequence deletion probabilities
+  var dj = "d0";
+  propagate(s, dj, -1);
+  for (var j = 1; j < hmm.numColumns; j++) {
+    var djprev = dj,
+        dj     = "d" + j;
+    propagate(djprev, dj, -1);
+  }
+  // compute one time transitions out of start state
+  var ij = "i" + 0,
+      ilast = "i" + hmm.numColumns;
+  propagate(s, ij, 0);
+  var mj = "m" + 0;
+  propagate(s, mj, 0);
+  // propagate probabilities via Viterbi recurrence relation and message passing
+  for (var i = 0; i < seq.length; i++) {
+    for (var j = 0; j < hmm.numColumns; j++) {
+      ij = "i" + j;
+      dj = "d" + j;
+      mj = "m" + j;
+      // all transitions out of insertion j
+      if (i > 0) {
+        propagate(ij, ij, i);
+        propagate(ij, mj, i);
+      }
+      propagate(ij, dj, i);
+      if (j < hmm.numColumns - 1) {
+        djnext = "d" + (j + 1);
+        mjnext = "m" + (j + 1);
+        // delete and merge transitions out of deletion j
+        propagate(dj, djnext, i);
+        propagate(dj, mjnext, i);
+        // delete and merge transitions out of match j
+        propagate(mj, djnext, i);
+        propagate(mj, mjnext, i);
+      }
+      ijnext = "i" + (j + 1);
+      // insertion transition out of delete j
+      propagate(dj, ijnext, i);
+      // insertion transition out of match j
+      propagate(mj, ijnext, i);
+    }
+    // insertion transition out of last transition
+    if (i > 0) {
+      propagate(ilast, ilast, i);
+    }
+  }
+  // compute one time transitions out of start state
+  var e = "e";
+  propagate(dj, e, seq.length);
+  propagate(ij, e, seq.length);
+  propagate(mj, e, seq.length);
+  // follow the pointers from the end state to the start state to get the path
+  return traceback(e, seq.length);
 }
 
 
@@ -605,6 +689,8 @@ Graph.msa = function(groups) {
   // 2) iteratively add each remaining track to the alignment
   for (var i = 1; i < groups.length; i++) {
     // a) align to HMM
+    var seq = groups[i].genes.map((g) => g.family);
+    Graph.MSAHMM.viterbi(hmm, seq);
     // b) update the transition and emission probabilities using the new alignment
     // c) if necessary, perform surgery on the graph
   }
