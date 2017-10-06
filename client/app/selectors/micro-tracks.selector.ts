e8a3df7ac52819bb44e48f57c66f912123083efb
Alan Cleary <alan.cleary@msu.montana.edu>
2017-10-06 11:28:33 -0600

    Added rest parameter syntax to micro-track filter to represent arbitrary number of filter arguments as array.

--- client/app/selectors/micro-tracks.selector.ts
+++ client/app/selectors/micro-tracks.selector.ts
@@ -3,10 +3,10 @@ import { MicroTracks } from '../models/micro-tracks.model';
 declare var Graph: any;
 
 export const microTracksSelector = (options?: any) => {
-  return state => state.map(([tracks, filters]) => {
+  return state => state.map(([tracks, ...filters]) => {
     let filteredTracks = Object.assign({}, tracks);
     if (filteredTracks !== undefined) {
-      for (let i = 1; i < filters.length; ++i) {
+      for (let i = 0; i < filters.length; ++i) {
         let f = filters[i];
         filteredTracks = f.algorithm(filteredTracks, options);
       }
