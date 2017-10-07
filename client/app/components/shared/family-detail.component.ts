062def0e62847141f01bdaf464fa752b2e70a6c6
adf-ncgr <adf@ncgr.org>
2017-10-06 22:29:17 -0600

    add link to pan-view with set of genes from family

--- client/app/components/shared/family-detail.component.ts
+++ client/app/components/shared/family-detail.component.ts
@@ -15,6 +15,7 @@ import { MicroTracks }    from '../../models/micro-tracks.model';
   selector: 'family-detail',
   template: `
     <h4>{{family.name}}</h4>
+    <p><a href="/index.html#/basic/{{gene_list}}">View genes in pan-view</a></p>
     <p><a href="http://legumeinfo.org/chado_gene_phylotree_v2?family={{family.name}}&gene_name={{gene_list}}">View genes in phylogram</a></p>
     <p>Genes:</p>
     <ul>
