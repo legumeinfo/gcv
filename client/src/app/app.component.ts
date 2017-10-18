595ef695ab9c8b326b1f56bdad2d2a34adc4b4ce
Alan Cleary <alan.cleary@msu.montana.edu>
2017-10-18 16:49:06 -0600

    Moved imports of global style sheets to app component for bundling purposes and changed path prefix for production build to be relative to index directory.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/styles.css';
+import '../assets/css/gcv.css';
+
 @Component({
   selector: 'gcv',
   template: `<router-outlet></router-outlet>`,
