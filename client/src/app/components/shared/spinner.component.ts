4a1264151706a81322a3455b52cf2c6621d7b3f4
Alan Cleary <alan.cleary@msu.montana.edu>
2017-10-18 16:34:28 -0600

    Moved spinner gif to pseudo-element of spinner class so it gets packed properly by webpack.

--- client/src/app/components/shared/spinner.component.ts
+++ client/src/app/components/shared/spinner.component.ts
@@ -6,7 +6,6 @@ import { Component, Input } from '@angular/core';
   template: `
     <div class="grey-screen" *ngIf="!data">
       <div class="spinner">
-        <img src="assets/img/spinner.gif" />
       </div>
     </div>
   `,
