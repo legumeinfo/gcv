90aa6a68cae1555d0572f619dcd6b057fb04eb1c
Alan Cleary <alan.cleary@msu.montana.edu>
2018-01-29 16:31:31 -0700

    Cleaned up params components.

--- client/src/app/components/multi/multi-params.component.ts
+++ client/src/app/components/multi/multi-params.component.ts
@@ -1,27 +1,16 @@
 // Angular
 import { Component,
          EventEmitter,
-         Input,
-         OnChanges,
-         OnDestroy,
          OnInit,
-         Output,
-         SimpleChanges }          from '@angular/core';
+         Output }                 from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Observable }             from 'rxjs/Observable';
 
 // App
-import { Alert }                  from '../../models/alert.model';
-import { Alerts }                 from '../../constants/alerts';
-import { AlertsService }          from '../../services/alerts.service';
-import { AppConfig }              from '../../app.config';
-import { ClusteringParams }       from '../../models/clustering-params.model';
-import { ClusteringService }      from '../../services/clustering.service';
-import { DefaultClusteringParams,
-         DefaultQueryParams }     from '../../constants/default-parameters';
-import { MicroTracksService }     from '../../services/micro-tracks.service';
-import { QueryParams }            from '../../models/query-params.model';
-//import { UrlService }             from '../../services/url-query-params.service';
+import { AppConfig }          from '../../app.config';
+import { ClusteringParams }   from '../../models/clustering-params.model';
+import { ClusteringService }  from '../../services/clustering.service';
+import { MicroTracksService } from '../../services/micro-tracks.service';
+import { QueryParams }        from '../../models/query-params.model';
 
 @Component({
   moduleId: module.id.toString(),
@@ -30,74 +19,41 @@ import { QueryParams }            from '../../models/query-params.model';
   styleUrls: [ 'multi-params.component.css' ]
 })
 
-//export class MultiParamsComponent implements OnChanges, OnDestroy, OnInit {
 export class MultiParamsComponent implements OnInit {
   // IO
-  @Input() queryGenes: string[];
   @Output() invalid = new EventEmitter();
-  @Output() submitted = new EventEmitter();
+  @Output() valid   = new EventEmitter();
 
   // UI
-
   help = false;
 
-  // data
-
+  // form groups
   queryGroup: FormGroup;
   clusteringGroup: FormGroup;
 
+  // form data
   sources = AppConfig.SERVERS.filter(s => s.hasOwnProperty('microMulti'));
 
-  //private _sub: any;
-
   // constructor
-
-  constructor(private _alerts: AlertsService,
-              private _clusteringService: ClusteringService,
+  constructor(private _clusteringService: ClusteringService,
               private _fb: FormBuilder,
               private _microTracksService: MicroTracksService) { }
 
   // Angular hooks
 
-  //ngOnChanges(changes: SimpleChanges): void {
-  //  if (this.queryGroup !== undefined)
-  //    this._multiQuery();
-  //}
-
-  //ngOnDestroy(): void {
-  //  this._sub.unsubscribe();
-  //}
-
   ngOnInit(): void {
     // initialize query group and subscribe to store updates
     let defaultQuery = new QueryParams();
-    this.queryGroup = this._fb.group(defaultQuery.formControls());
-    this._microTracksService.queryParams.subscribe(params => {
-      this._updateGroup(this.queryGroup, params);
-    });
+    this.queryGroup  = this._fb.group(defaultQuery.formControls());
+    this._microTracksService.queryParams
+      .subscribe(params => this._updateGroup(this.queryGroup, params));
 
     // initialize clustering group and subscribe to store updates
     let defaultClustering = new ClusteringParams();
-    this.clusteringGroup = this._fb.group(defaultClustering.formControls());
-    this._clusteringService.clusteringParams.subscribe(params => {
-      this._updateGroup(this.queryGroup, params);
-    });
+    this.clusteringGroup  = this._fb.group(defaultClustering.formControls());
+    this._clusteringService.clusteringParams
+      .subscribe(params => this._updateGroup(this.queryGroup, params));
 
-    // subscribe to url query param updates
-    //this._sub = this._url.params.subscribe(params => {
-    //  let oldQuery = this.queryGroup.getRawValue();
-    //  this.queryGroup.patchValue(params);
-    //  let newQuery = this.queryGroup.getRawValue();
-    //  if (JSON.stringify(oldQuery) !== JSON.stringify(newQuery))
-    //    this.queryGroup.markAsDirty();
-    //  let oldClustering = this.clusteringGroup.getRawValue();
-    //  this.clusteringGroup.patchValue(params);
-    //  let newClustering = this.clusteringGroup.getRawValue();
-    //  if (JSON.stringify(oldClustering) !== JSON.stringify(newClustering))
-    //    this.clusteringGroup.markAsDirty();
-    //  if (this.queryGroup.dirty || this.clusteringGroup.dirty)
-    //    this.submit();
-    //});
     // submit the updated form
     this.queryGroup.markAsDirty();
     this.clusteringGroup.markAsDirty();
@@ -110,21 +66,11 @@ export class MultiParamsComponent implements OnInit {
     group.patchValue(params);
   }
 
-  //private _multiQuery(): void {
-  //  this._microTracksService.multiQuery(
-  //    this.queryGenes,
-  //    this.queryGroup.getRawValue(),
-  //    e => this._alerts.pushAlert(new Alert(Alerts.ALERT_DANGER, e))
-  //  );
-  //}
-
   private _submitGroup(group, callback=params=>{}): void {
     if (group.dirty) {
-      //this.submitted.emit();
       let params = group.getRawValue();
       callback(params);
       group.reset(params);
-      //this._url.updateParams(Object.assign({}, params));
     }
   }
 
@@ -132,6 +78,7 @@ export class MultiParamsComponent implements OnInit {
 
   submit(): void {
     if (this.queryGroup.valid && this.clusteringGroup.valid) {
+      this.valid.emit();
       // submit query params
       this._submitGroup(this.queryGroup, params => {
         this._microTracksService.updateParams(params);
--- client/src/app/components/search/search-params.component.ts
+++ client/src/app/components/search/search-params.component.ts
@@ -1,30 +1,19 @@
 // Angular
-import { BehaviorSubject }        from 'rxjs/BehaviorSubject';
 import { Component,
          EventEmitter,
-         Input,
-         OnChanges,
-         OnDestroy,
          OnInit,
-         Output,
-         SimpleChanges }          from '@angular/core';
+         Output }                 from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 // App services
-import { Alert }                 from '../../models/alert.model';
-import { Alerts }                from '../../constants/alerts';
-import { AlertsService }         from '../../services/alerts.service';
-import { ALIGNMENT_ALGORITHMS }  from '../../constants/alignment-algorithms';
-import { AlignmentParams }       from '../../models/alignment-params.model';
-import { AlignmentService }      from '../../services/alignment.service';
-import { AppConfig }             from '../../app.config';
-import { BlockParams }           from '../../models/block-params.model';
-import { DefaultAlignmentParams, DefaultBlockParams,
-         DefaultQueryParams }    from '../../constants/default-parameters';
-import { MacroTracksService }    from '../../services/macro-tracks.service';
-import { MicroTracksService }    from '../../services/micro-tracks.service';
-import { QueryParams }           from '../../models/query-params.model';
-//import { UrlService }            from '../../services/url.service';
+import { ALIGNMENT_ALGORITHMS } from '../../constants/alignment-algorithms';
+import { AlignmentParams }      from '../../models/alignment-params.model';
+import { AlignmentService }     from '../../services/alignment.service';
+import { AppConfig }            from '../../app.config';
+import { BlockParams }          from '../../models/block-params.model';
+import { MacroTracksService }   from '../../services/macro-tracks.service';
+import { MicroTracksService }   from '../../services/micro-tracks.service';
+import { QueryParams }          from '../../models/query-params.model';
 
 @Component({
   moduleId: module.id.toString(),
@@ -33,85 +22,52 @@ import { QueryParams }           from '../../models/query-params.model';
   styleUrls: [ 'search-params.component.css' ]
 })
 
-//export class SearchParamsComponent implements OnChanges, OnDestroy, OnInit {
 export class SearchParamsComponent implements OnInit {
 
   // component IO
-
-  @Input() source: string;
-  @Input() gene: string;
   @Output() invalid = new EventEmitter();
-  @Output() submitted = new EventEmitter();
+  @Output() valid   = new EventEmitter();
 
-  // component variables
-
-  blockHelp = false;
-  queryHelp = false;
+  // UI state
+  blockHelp     = false;
+  queryHelp     = false;
   alignmentHelp = false;
 
+  // form groups
   blockGroup: FormGroup;
   queryGroup: FormGroup;
   alignmentGroup: FormGroup;
 
-  sources = AppConfig.SERVERS.filter(s => s.hasOwnProperty('microSearch'));
+  // form data
+  sources    = AppConfig.SERVERS.filter(s => s.hasOwnProperty('microSearch'));
   algorithms = ALIGNMENT_ALGORITHMS;
 
-  //private _sub: any;
-
   // constructor
-
-  constructor(private _alerts: AlertsService,
-              private _alignmentService: AlignmentService,
+  constructor(private _alignmentService: AlignmentService,
               private _fb: FormBuilder,
               private _macroTracksService: MacroTracksService,
               private _microTracksService: MicroTracksService) { }
 
   // Angular hooks
 
-  //ngOnChanges(changes: SimpleChanges) {
-  //  if (this.queryGroup !== undefined)
-  //    this._geneSearch();
-  //}
-
-  //ngOnDestroy(): void {
-  //  this._sub.unsubscribe();
-  //}
-
   ngOnInit(): void {
     // initialize block group and subscribe to store updates
     let defaultBlock = new BlockParams();
-    this.blockGroup = this._fb.group(defaultBlock.formControls());
-    this._macroTracksService.blockParams.subscribe(params => {
-      this._updateGroup(this.blockGroup, params);
-    });
+    this.blockGroup  = this._fb.group(defaultBlock.formControls());
+    this._macroTracksService.blockParams
+      .subscribe(params => this._updateGroup(this.blockGroup, params));
 
     // initialize query group and subscribe to store updates
     let defaultQuery = new QueryParams();
-    this.queryGroup = this._fb.group(defaultQuery.formControls());
-    this._microTracksService.queryParams.subscribe(params => {
-      this._updateGroup(this.queryGroup, params);
-    });
+    this.queryGroup  = this._fb.group(defaultQuery.formControls());
+    this._microTracksService.queryParams
+      .subscribe(params => this._updateGroup(this.queryGroup, params));
 
     // initialize alignment group and subscribe to store updates
     let defaultAlignment = new AlignmentParams();
-    this.alignmentGroup = this._fb.group(defaultAlignment.formControls());
-    this._alignmentService.alignmentParams.subscribe(params => {
-      this._updateGroup(this.alignmentGroup, params);
-    });
-
-    // subscribe to url query param updates
-    //this._sub = this._url.params.subscribe(params => {
-    //  // update block params
-    //  this._paramUpdateGroup(this.blockGroup, params);
-    //  // update query params
-    //  this._paramUpdateGroup(this.queryGroup, params);
-    //  // update alignment params
-    //  this._paramUpdateGroup(this.alignmentGroup, params);
-    //  // resubmit the params form if necessary
-    //  if (this.blockGroup.dirty || this.queryGroup.dirty ||
-    //  this.alignmentGroup.dirty)
-    //    this.submit();
-    //});
+    this.alignmentGroup  = this._fb.group(defaultAlignment.formControls());
+    this._alignmentService.alignmentParams
+      .subscribe(params => this._updateGroup(this.alignmentGroup, params));
 
     // submit the updated form
     this.blockGroup.markAsDirty();
@@ -122,35 +78,16 @@ export class SearchParamsComponent implements OnInit {
 
   // private
 
-  private _updateGroup(group, params) {
-    group.patchValue(params);
-  }
-
-  //private _geneSearch(): void {
-  //  this._microTracksService.geneSearch(
-  //    this.source,
-  //    this.gene,
-  //    this.queryGroup.getRawValue(),
-  //    e => this._alerts.pushAlert(new Alert(Alerts.ALERT_DANGER, e))
-  //  );
-  //}
-
-  //private _paramUpdateGroup(group, params): void {
-  //  let oldValue = group.getRawValue();
-  //  group.patchValue(params);
-  //  let newValue = group.getRawValue();
-  //  if (JSON.stringify(oldValue) !== JSON.stringify(newValue))
-  //    group.markAsDirty();
-  //}
-
   private _submitGroup(group, callback=params=>{}): void {
     if (group.dirty) {
-      //this.submitted.emit();
       let params = group.getRawValue();
       callback(params);
       group.reset(params);
-      //this._url.updateParams(Object.assign({}, params));
     }
+  }
+
+  private _updateGroup(group, params) {
+    group.patchValue(params);
   }
 
   // public
@@ -158,14 +95,13 @@ export class SearchParamsComponent implements OnInit {
   submit(): void {
     if (this.blockGroup.valid && this.queryGroup.valid &&
     this.alignmentGroup.valid) {
+      this.valid.emit();
       // submit block params
       this._submitGroup(this.blockGroup, params => {
         this._macroTracksService.updateParams(params);
       });
       // submit query params
       this._submitGroup(this.queryGroup, params => {
-        //this.submitted.emit();
-        //this._geneSearch()
         this._microTracksService.updateParams(params);
       });
       // submit alignment params
