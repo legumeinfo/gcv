436c0ea34ffcc9802eac74d64ef7bca32f1d0d65
Alan Cleary <alan.cleary@msu.montana.edu>
2018-03-06 15:29:15 -0700

    Replaced class variable with use of the scan operator in the navigation router effect for accumulating query parameters.

--- client/src/app/effects/router.effects.ts
+++ client/src/app/effects/router.effects.ts
@@ -4,14 +4,11 @@ import { Router } from "@angular/router";
 import { Location } from "@angular/common";
 // store
 import { Effect, Actions, ofType } from "@ngrx/effects";
-import { map, tap } from "rxjs/operators";
+import { map, scan, tap } from "rxjs/operators";
 import * as routerActions from "../actions/router.actions";
 
 @Injectable()
 export class RouterEffects {
-
-  // TODO: update navigate effect so it doesn't rely on this variable
-  private queryParams: any = {};
 
   constructor(
     private actions$: Actions,
@@ -23,10 +20,13 @@ export class RouterEffects {
   navigate$ = this.actions$.pipe(
     ofType(routerActions.GO),
     map((action: routerActions.Go) => action.payload),
+    scan((currentRoute, {path, query: queryParams, extras}) => {
+      const nextQueryParams = Object.assign({}, currentRoute.query, queryParams);
+      return {path, query: nextQueryParams, extras};
+    }, {path: [], query: {}, extras: {}}),
     tap(({path, query: queryParams, extras}) => {
-      Object.assign(this.queryParams, queryParams);
       this.router.navigate(path, {
-        queryParams: this.queryParams,
+        queryParams,
         queryParamsHandling: "merge",
         ...extras,
       });
