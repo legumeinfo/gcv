ca72acad33c3405fba0645f932804311084011a7
Alan Cleary <alan.cleary@msu.montana.edu>
2018-06-05 23:08:53 -0600

    Removed unused constants.

--- client/src/app/constants/alerts.ts
+++
@@ -1,6 +0,0 @@
-export const enum Alerts {
-  ALERT_SUCCESS = 'alert-success',
-  ALERT_INFO    = 'alert-info',
-  ALERT_WARNING = 'alert-warning',
-  ALERT_DANGER  = 'alert-danger'
-}
--- client/src/app/constants/app-routes.ts
+++
@@ -1,4 +0,0 @@
-export const enum AppRoutes {
-  SEARCH = 'search',
-  MULTI  = 'multi'
-}
