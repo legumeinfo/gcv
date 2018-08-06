89da7579b89deac373965e4a8d63c7038812adfd
Alan Cleary <alan.cleary@msu.montana.edu>
2018-08-06 11:08:27 -0600

    Updated the tour service so that only route changes where the path changes trigger the tour. Also, the tour trigger now handles tours that have already been initialized and those that have not differently. This prevents an already initialized tour from being initialized again, which fixes some unexpected behavior that was being observed, namely, the arrow keys skipping steps.

--- client/src/app/services/tour.service.ts
+++ client/src/app/services/tour.service.ts
@@ -1,8 +1,8 @@
 // Angular
 import { Injectable } from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
+import { PRIMARY_OUTLET, NavigationEnd, Router } from "@angular/router";
 // store
-import { filter } from "rxjs/operators";
+import { distinctUntilChanged, filter } from "rxjs/operators";
 // app
 import { AppConfig } from "../app.config";
 
@@ -11,12 +11,24 @@ export class TourService {
 
   constructor(private router: Router) {
     router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        distinctUntilChanged((a: any, b: any) => {
+          const aPath = this.urlToPath(a.url);
+          const bPath = this.urlToPath(b.url);
+          return aPath === bPath;
+        })
+        )
       .subscribe((event) => {
         // ensure the tour is initialized after every route change so the next
         // step can be shown
         for (const name of AppConfig.TOURS) {
-          window[name].init();
+          if (window[name]._inited) {
+            const i = window[name].getCurrentStep();
+            window[name].showStep(i);
+          } else {
+            window[name].init();
+          }
         }
       });
   }
@@ -26,5 +38,11 @@ export class TourService {
       window[name].end();
       window[name].restart();
     }
+  }
+
+  private urlToPath(url: string) {
+    const tree = this.router.parseUrl(url);
+    const path = tree.root.children[PRIMARY_OUTLET].toString();
+    return path;
   }
 }
