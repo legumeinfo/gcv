0c8c566a0fab2e9b8b8c8d4c83c5a566b9004fea
Alan Cleary <alan.cleary@msu.montana.edu>
2018-08-09 11:01:42 -0600

    Updated how Angular's routing mechanism is made available outside of GCV so that route changes triggered externally are always executed within the Angular zone.

--- client/src/app/components/app.component.ts
+++ client/src/app/components/app.component.ts
@@ -1,5 +1,5 @@
 // Angular
-import { Component } from "@angular/core";
+import { Component, NgZone } from "@angular/core";
 import { PRIMARY_OUTLET, Router } from "@angular/router";
 // store
 import { Store } from "@ngrx/store";
@@ -17,17 +17,26 @@ declare var window: any;
 export class AppComponent {
   constructor(private router: Router,
               private store: Store<fromRoot.State>,
-              private tourService: TourService) {  // resume tours from anywhere
-    // make single page app navigation available outside of Angular
-    window.gcvGo = (url) => {
-      const tree = router.parseUrl(url);
+              private tourService: TourService,  // resume tours from anywhere
+              private zone: NgZone) {
+    // make the app's single page navigation available outside of Angular, but
+    // ensure that the function always executes in the AppComponent context by
+    // binding this
+    window.gcvGo = this.navigate.bind(this);
+  }
+
+  // performs an Angular navigation while ensuring that it's executed in within
+  // the Angular zone
+  private navigate(url): void {
+    this.zone.run(() => {
+      const tree = this.router.parseUrl(url);
       const path = tree.root.children[PRIMARY_OUTLET].toString();
       const queryParams = tree.queryParams;
-      store.dispatch(new routerActions.Go({
+      this.store.dispatch(new routerActions.Go({
         path: [path],
         query: queryParams,
-        extras: { replaceUrl: false }
+        extras: {replaceUrl: false}
       }));
-    };
+    });
   }
 }
