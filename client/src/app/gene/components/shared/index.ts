02020617a46feb555d84565670622473dc82d65a
Alan Cleary <alan.cleary@msu.montana.edu>
2019-11-15 16:23:31 -0700

    Removed deprecated toggle button component.

--- client/src/app/gene/components/shared/index.ts
+++ client/src/app/gene/components/shared/index.ts
@@ -1,16 +1,13 @@
 import { ContextMenuComponent } from './context-menu.component';
 import { OrderingComponent } from './ordering.component';
 import { RegexpComponent } from './regexp.component';
-import { ToggleButtonComponent } from './toggle-button.component';
 
 export const components: any[] = [
   ContextMenuComponent,
   OrderingComponent,
   RegexpComponent,
-  ToggleButtonComponent,
 ];
 
 export * from './context-menu.component';
 export * from './ordering.component';
 export * from './regexp.component';
-export * from './toggle-button.component';
