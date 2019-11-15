5338e95c8f4fccb352746ac903e5520fd38f9a33
Alan Cleary <alan.cleary@msu.montana.edu>
2019-11-15 16:16:13 -0700

    Removed deprecated main and main content components.

--- client/src/app/gene/components/shared/index.ts
+++ client/src/app/gene/components/shared/index.ts
@@ -1,22 +1,16 @@
 import { ContextMenuComponent } from './context-menu.component';
-import { MainContentComponent } from './main-content.component';
-import { MainComponent } from './main.component';
 import { OrderingComponent } from './ordering.component';
 import { RegexpComponent } from './regexp.component';
 import { ToggleButtonComponent } from './toggle-button.component';
 
 export const components: any[] = [
   ContextMenuComponent,
-  MainContentComponent,
-  MainComponent,
   OrderingComponent,
   RegexpComponent,
   ToggleButtonComponent,
 ];
 
 export * from './context-menu.component';
-export * from './main-content.component';
-export * from './main.component';
 export * from './ordering.component';
 export * from './regexp.component';
 export * from './toggle-button.component';
--- client/src/app/gene/components/shared/main.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-main',
-  styleUrls: [ './main.component.scss' ],
-  templateUrl: './main.component.html',
-})
-export class MainComponent { }
