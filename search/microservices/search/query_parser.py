fe1d454845f293ba13fecf8137371ac5221b7ab5
Alan Cleary <alan.cleary@msu.montana.edu>
2020-09-02 12:38:23 -0600

    Updated the query parser of the search microservice to support more standard region notations.

--- search/microservices/search/query_parser.py
+++ search/microservices/search/query_parser.py
@@ -4,10 +4,10 @@ from pyparsing import Group, Optional, Suppress, Word, ZeroOrMore, alphanums, nu
 def makeQueryParser(name_special_characters=''):
   uint = Word(nums).setParseAction(lambda toks: int(toks[0]))
   name = Word(alphanums + name_special_characters)
-  interval = uint + Suppress('-') + uint
+  interval = uint + Suppress(Group('-')|Group('..')) + uint
   gene = Group(Suppress(Optional('gene:')) + name)\
              .setResultsName('genes', listAllMatches=True)
-  region = Group(Suppress(Optional('region:')) + name + interval)\
+  region = Group(Suppress(Optional('region:')) + name + Suppress(Optional(':')) + interval)\
                .setResultsName('regions', listAllMatches=True)
   parser = ZeroOrMore(region | gene)
   return parser
@@ -22,6 +22,15 @@ if __name__ == '__main__':
     'phavu.Chr02 10-123',
     'region:phavu.Chr02 432-2415',
     'region: phavu.Chr02 432-2415',
+    'phavu.Chr02 432 -2415',
+    'phavu.Chr02 432 - 2415',
+    'phavu.Chr02 10..123',
+    'phavu.Chr02 432 ..2415',
+    'phavu.Chr02 432 .. 2415',
+    'phavu.Chr02:432-2415',
+    'phavu.Chr02:432..2415',
+    'phavu.Chr02: 432..2415',
+    'region:phavu.Chr02 432 .. -2415',
     'phavu.Phvul.002G085200 phavu.Chr02 10-123  phavu.Chr02 10-123',
   ]
   for query in test_queries:
