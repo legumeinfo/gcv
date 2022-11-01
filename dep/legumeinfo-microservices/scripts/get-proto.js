9adaf7d9176aff2ab035796771ccede757822f44
Alan Cleary <acleary@ncgr.org>
2022-11-01 15:02:39 -0600

    Updated the microservices pseudo-package to use the most recent microservice versions.

--- dep/legumeinfo-microservices/scripts/get-proto.js
+++ dep/legumeinfo-microservices/scripts/get-proto.js
@@ -13,22 +13,22 @@ const repo = 'legumeinfo/microservices'
 
 // each microservice's subdirectory name and version required by GCV
 const microservices = [
-  {name: 'chromosome', version: '1.1.0'},
-  {name: 'chromosome_region', version: '1.1.0'},
-  {name: 'chromosome_search', version: '1.1.0'},
-  {name: 'genes', version: '1.1.0'},
-  {name: 'gene_search', version: '1.1.0'},
-  {name: 'macro_synteny_blocks', version: '1.3.0'},
-  {name: 'micro_synteny_search', version: '1.1.0'},
-  {name: 'pairwise_macro_synteny_blocks', version: '1.3.0'},
-  {name: 'search', version: '1.1.0'},
+  {name: 'chromosome', version: '1.1.2'},
+  {name: 'chromosome_region', version: '1.1.2'},
+  {name: 'chromosome_search', version: '1.1.2'},
+  {name: 'genes', version: '1.1.2'},
+  {name: 'gene_search', version: '1.1.2'},
+  {name: 'macro_synteny_blocks', version: '1.3.2'},
+  {name: 'micro_synteny_search', version: '1.1.2'},
+  {name: 'pairwise_macro_synteny_blocks', version: '1.3.2'},
+  {name: 'search', version: '1.1.2'},
 ];
 
 // where the files will be saved
 const destination = path.resolve('./proto');
 
 // degit options
-const options = {cache: false, force: true, verbose: false};
+const options = {disableCache: true, force: true, verbose: false};
 
 // fetch each microservice's .proto files individually
 const promises = microservices.map(({name, version}) => {
