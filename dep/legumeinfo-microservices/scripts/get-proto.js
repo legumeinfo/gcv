f21e1848a9d501d51425ff8f7e5bbf79b3fb6348
Alan Cleary <acleary@ncgr.org>
2023-01-25 09:33:59 -0700

    Updated microservice dependencies.

--- dep/legumeinfo-microservices/scripts/get-proto.js
+++ dep/legumeinfo-microservices/scripts/get-proto.js
@@ -13,15 +13,15 @@ const repo = 'legumeinfo/microservices'
 
 // each microservice's subdirectory name and version required by GCV
 const microservices = [
-  {name: 'chromosome', version: '1.1.2'},
-  {name: 'chromosome_region', version: '1.1.2'},
-  {name: 'chromosome_search', version: '1.1.2'},
-  {name: 'genes', version: '1.1.2'},
-  {name: 'gene_search', version: '1.1.2'},
-  {name: 'macro_synteny_blocks', version: '1.3.2'},
-  {name: 'micro_synteny_search', version: '1.1.2'},
-  {name: 'pairwise_macro_synteny_blocks', version: '1.3.2'},
-  {name: 'search', version: '1.1.2'},
+  {name: 'chromosome', version: '1.1.3'},
+  {name: 'chromosome_region', version: '1.1.3'},
+  {name: 'chromosome_search', version: '1.1.3'},
+  {name: 'genes', version: '1.1.3'},
+  {name: 'gene_search', version: '1.1.3'},
+  {name: 'macro_synteny_blocks', version: '1.3.3'},
+  {name: 'micro_synteny_search', version: '1.1.3'},
+  {name: 'pairwise_macro_synteny_blocks', version: '1.3.3'},
+  {name: 'search', version: '1.1.3'},
 ];
 
 // where the files will be saved
